106020a85d0794418606106b15962d7c4bd11533
Reinier Romero Mir <nenirey@gmail.com>
2022-11-27 11:13:06 -0500

    update server.py

--- server.py
+++ server.py
@@ -1,11 +1,27 @@
 from flask import Flask, request
 import os
+import telebot
 
-app = Flask(__name__)
+TOKEN = os.environ.get('TOKEN')
+WEBHOOK = os.environ.get('WEBHOOK')
 
-@app.route("/")
-def webhook():
-    return "!", 200
+bot = telebot.TeleBot(token=TOKEN, skip_pending=False)
+server = Flask(__name__)
+
+@bot.message_handler(commands=['help','Help','HELP','hELP'])
+def send_welcome(message):
+    bot.reply_to(message, 'This is a simplebot_tg helper')
+
+@server.route('/' + TOKEN, methods=['POST'])
+   def getMessage():
+       bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+       return "!", 200
+
+   @server.route("/")
+   def webhook():
+       bot.remove_webhook()
+       bot.set_webhook(url=WEBHOOK + TOKEN, allowed_updates=util.update_types, drop_pending_updates = False)
+       return "!", 200
        
 app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 10000)))
 
