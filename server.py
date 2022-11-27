eaf9d551f5d14e80a3af14896c49b9701ab57cb6
Reinier Romero Mir <nenirey@gmail.com>
2022-11-27 11:47:59 -0500

    update server.py

--- server.py
+++ server.py
@@ -13,15 +13,15 @@ def send_welcome(message):
     bot.reply_to(message, 'This is a simplebot_tg helper')
 
 @server.route('/' + TOKEN, methods=['POST'])
-   def getMessage():
-       bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-       return "!", 200
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
 
-   @server.route("/")
-   def webhook():
-       bot.remove_webhook()
-       bot.set_webhook(url=WEBHOOK + TOKEN, allowed_updates=util.update_types, drop_pending_updates = False)
-       return "!", 200
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url=WEBHOOK + TOKEN, allowed_updates=util.update_types, drop_pending_updates = False)
+    return "!", 200
        
 app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 10000)))
 
