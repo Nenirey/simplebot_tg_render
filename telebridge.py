76b0597bde36faa24e28a6d8f7f95478b71bca78
Reinier Romero Mir <nenirey@gmail.com>
2023-01-22 12:40:28 -0500

    update telebridge.py

--- telebridge.py
+++ telebridge.py
@@ -1642,7 +1642,8 @@ async def load_chat_messages(bot: DeltaBot, message = Message, replies = Replies
        all_chats = await client.get_dialogs()
        tchat = None
        for ch in all_chats:
-           if str(ch.entity.id) == str(target).lstrip('-100'):
+           print(str(ch.entity.id)+" "+str(target).lstrip('-100'))
+           if str(ch.entity.id) == str(target).lstrip('-100') or ch.entity.id == target:
               tchat = ch
            elif hasattr(ch.entity,'username') and str(ch.entity.username) == str(target):
               tchat = ch
